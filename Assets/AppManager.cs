f70d57f4b163572109e7aa9552a57ea17aa468b6
JooYoo <jooyoo@outlook.com>
2018-05-04 13:00:24 +0200

    create project

---
+++ Assets/AppManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AppQuit()
+    {
+        Application.Quit();
+        Debug.Log("Quit Clicked");
+    }
+}
