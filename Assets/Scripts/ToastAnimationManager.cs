38db9a48ff025a67ed8001032ef968ede416d353
JooYoo <jooyoo@outlook.com>
2018-05-07 11:11:36 +0200

    added toasts

---
+++ Assets/Scripts/ToastAnimationManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ToastAnimationManager : MonoBehaviour
+{
+    private Animator toastAnimator;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    toastAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.O))
+	    {
+	        toastAnimator.SetTrigger("toastsPop");
+	    }
+	}
+}
