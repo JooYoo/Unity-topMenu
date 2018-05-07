0bc63b6baa1eebc6782432be0a7cdf269c9eadac
JooYoo <jooyoo@outlook.com>
2018-05-07 10:16:09 +0200

    added Indicator

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    private Animator animator;
+    private bool isRec;
+
+	void Start ()
+	{
+	    animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        isRec = !isRec;
+
+	        if (isRec) // Space: Start Rec
+	        {
+	            animator.SetBool("StartRec", true);
+	            animator.SetBool("StopRec", false);
+	        }
+
+	        if (!isRec) // Space: Stop Rec 
+	        {
+	            animator.SetBool("StopRec", true);
+	            animator.SetBool("StartRec", false);
+	        }
+        }
+	}
+}
